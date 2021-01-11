aa5c40ffc5668ab24c1ecfe8c713559f26d352a8
Arvin Zarei <arvinzarei123@gmail.com>
2021-01-12 00:16:23 +0330

    Create index file and seperate sections of site

---
+++ index.php
@@ -0,0 +1,16 @@
+<!-- Start Header -->
+<?php include_once "_header.php" ?>
+  <!-- End Header  -->
+
+<body>
+
+<!-- Start Create Menu -->
+<?php include_once "_menu.php"?>
+<!-- End Menu -->
+
+<!-- Start JS -->
+<?php include_once "_js.js" ?>
+<!-- End JS -->  
+
+</body>
+</html>
