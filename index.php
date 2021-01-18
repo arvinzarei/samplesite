30a4f478540619bbbce893420e88afb42a8f1a16
Arvin Zarei <arvinzarei123@gmail.com>
2021-01-18 23:35:20 +0330

    Create Slider section

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
 <!-- Start Header -->
-<?php include_once "_header.php" ?>
+<?php include_once "_header.php"; ?>
   <!-- End Header  -->
 
 <body>
 
 <!-- Start Create Menu -->
-<?php include_once "_menu.php"?>
+<?php include_once "_menu.php"; ?>
 <!-- End Menu -->
 
+<!-- Start Slider -->
+<?php include_once '_slider.php';?>
+<!-- End Slider -->
+
 <!-- Start JS -->
-<?php include_once "_js.js" ?>
+<?php include_once "_js.php"; ?>
 <!-- End JS -->  
 </body>
 </html>
